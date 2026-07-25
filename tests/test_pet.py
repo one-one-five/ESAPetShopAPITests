988c0c5f270165a2360936d7d11ba0e17134a72b
null <s.a.elizaroff@gmail.com>
2026-07-25 09:29:52 +0300

    add test_add_new_pet ('Добавление нового питомца c полными данными')

--- tests/test_pet.py
+++ tests/test_pet.py
@@ -1,5 +1,7 @@
 import allure
 import httpx
+from jsonschema import validate
+from .schema.pet_json import PET_JSON
 
 BASE_URL = 'http://5.181.109.28:9090/api/v3'
 
@@ -38,3 +40,35 @@ class TestPet:
             assert response.status_code == 404, 'Код ответа не совпал с ожидаемым'
         with allure.step('Проверка текстового содержимого ответа'):
             assert response.text == 'Pet not found', 'Текст ошибки не совпал с ожидаемым'
+
+    @allure.title('Добавление нового питомца c полными данными')
+    def test_add_new_pet(self):
+        body = {
+            "id": 77
+            , "name": "doggie"
+            , "category": {
+                "id": 88
+                , "name": "Dogs"
+            }
+            , "photoUrls": ["string"]
+            , "tags": [
+                {
+                    "id": 99
+                    , "name": "string"
+                }
+            ]
+            , "status": "available"
+        }
+        response = httpx.post(url=f'{BASE_URL}/pet', json=body)
+
+        with allure.step('Отправка запроса на получение информации о несуществующем питомце'):
+            with allure.step('Проверка статуса ответа'):
+                assert response.status_code == 200, 'Код ответа не совпал с ожидаемым'
+
+            response_json = response.json()
+
+            with allure.step('Валидация JSON схемы'):
+                validate(response_json, PET_JSON)
+
+            with allure.step('Проверка полей ответа'):
+                assert response_json == body
