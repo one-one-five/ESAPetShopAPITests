61c89426f25f1126bafc9c91a58a129e7df96ac2
null <s.a.elizaroff@gmail.com>
2026-07-25 09:30:17 +0300

    add test_add_new_pet ('Добавление нового питомца c полными данными')

---
+++ tests/schema/pet_json.py
@@ -0,0 +1,69 @@
+PET_JSON = {
+    "type": "object",
+    "properties": {
+        "id": {
+            "type": "integer"
+        },
+        "name": {
+            "type": "string"
+        },
+        "category": {
+            "type": "object",
+            "properties": {
+                "id": {
+                    "type": "integer"
+                },
+                "name": {
+                    "type": "string"
+                }
+            },
+            "required": [
+                "id",
+                "name"
+            ],
+            "additionalProperties": False
+        },
+        "photoUrls": {
+            "type": "array",
+            "items": {
+                "type": "string"
+            }
+        },
+        "tags": {
+            "type": "array",
+            "items": {
+                "type": "object",
+                "properties": {
+                    "id": {
+                        "type": "integer"
+                    },
+                    "name": {
+                        "type": "string"
+                    }
+                },
+                "required": [
+                    "id",
+                    "name"
+                ],
+                "additionalProperties": False
+            }
+        },
+        "status": {
+            "type": "string",
+            "enum": [
+                "available",
+                "pending",
+                "sold"
+            ]
+        }
+    },
+    "required": [
+        "id",
+        "name",
+        "category",
+        "photoUrls",
+        "tags",
+        "status"
+    ],
+    "additionalProperties": False
+}
