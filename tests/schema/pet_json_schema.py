c0accc3a7b10c41660e681adae015aa1a2af3946
null <s.a.elizaroff@gmail.com>
2026-07-26 10:07:00 +0300

    -

---
+++ tests/schema/pet_json_schema.py
@@ -0,0 +1,67 @@
+PET_JSON = {
+    "type": "object",
+    "properties": {
+        "id": {
+            "type": "integer"
+        },
+        "name": {
+            "type": "string"
+        },
+        "category": {
+            "type": "object",
+            "properties": {
+                "id": {
+                    "type": "integer"
+                },
+                "name": {
+                    "type": "string"
+                }
+            },
+            "required": [
+                "id",
+                "name"
+            ],
+            "additionalProperties": False
+        },
+        "photoUrls": {
+            "type": "array",
+            "items": {
+                "type": "string"
+            }
+        },
+        "tags": {
+            "type": "array",
+            "items": {
+                "type": "object",
+                "properties": {
+                    "id": {
+                        "type": "integer"
+                    },
+                    "name": {
+                        "type": "string"
+                    }
+                },
+                "required": [
+                    "id",
+                    "name"
+                ],
+                "additionalProperties": False
+            }
+        },
+        "status": {
+            "type": "string",
+            "enum": [
+                "available",
+                "pending",
+                "sold"
+            ]
+        }
+    },
+    "required": [
+        "id",
+        "name",
+        "photoUrls",
+        "status"
+    ],
+    "additionalProperties": False
+}
