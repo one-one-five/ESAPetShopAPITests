d447078b562b6ba8d213694e7ab5b25378c6538d
null <s_a_elizarov@mail.ru>
2026-07-31 17:03:11 +0300

    1. исправил двойное присовение в store_json_schema.py 2. добавил новую схему размещения заказа 3. в тесте test_placing_an_order ('Размещение заказа (#42)') убрал использование фикстуры, добавил валидацию Схемы + проверку что ответ содержит переданные данные

--- tests/schema/store_json_schema.py
+++ tests/schema/store_json_schema.py
@@ -1,4 +1,4 @@
-STORE_JSON = STORE_JSON = {
+STORE_JSON = {
     "type": "object",
     "properties": {
         "approved": {
@@ -15,5 +15,36 @@ STORE_JSON = STORE_JSON = {
         }
     },
     "required": ["approved", "delivered", "placed"],
+    "additionalProperties": False
+}
+
+ORDER_JSON = {
+    "type": "object",
+    "properties": {
+        "id": {
+            "type": "integer",
+            "format": "int64"
+        },
+        "petId": {
+            "type": "integer",
+            "format": "int64"
+        },
+        "quantity": {
+            "type": "integer",
+            "format": "int32"
+        },
+        "shipDate": {
+            "type": "string",
+            "format": "date-time"
+        },
+        "status": {
+            "type": "string",
+            "enum": ["placed", "approved", "delivered"]
+        },
+        "complete": {
+            "type": "boolean"
+        }
+    },
+    "required": ["id", "petId", "quantity", "status", "complete"],
     "additionalProperties": False
 }
